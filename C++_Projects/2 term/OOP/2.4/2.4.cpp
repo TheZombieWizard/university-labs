f850fb1f1a48cfa95f818ee246ab0364b1328e2b
ZombieWizard <64956872+TheZombieWizard@users.noreply.github.com>
2023-04-04 21:41:53 +0300

    Updated OOP 2.4.cpp

--- C++_Projects/2 term/OOP/2.4/2.4.cpp
+++ C++_Projects/2 term/OOP/2.4/2.4.cpp
@@ -5,17 +5,31 @@ int main()
     RationalFraction ratfrac1(1, 2);
     RationalFraction ratfrac2(1, 3);
     cout << ratfrac1 << endl;
+    cin >> ratfrac1;
+    cout << ratfrac1 << endl;
     cout << ratfrac2 << endl;
+    
+    cout << endl << "Operations check: " << endl;
     ratfrac2 += ratfrac1;
+    ratfrac2.reduce2nonreducable();
     cout << ratfrac2 << endl;
+    
     ratfrac2 -= ratfrac1;
+    ratfrac2.reduce2nonreducable();
     cout << ratfrac2 << endl;
+    
     ratfrac2 *= ratfrac1;
+    ratfrac2.reduce2nonreducable();
     cout << ratfrac2 << endl;
+    
     ratfrac2 /= ratfrac1;
+    ratfrac2.reduce2nonreducable();
     cout << ratfrac2 << endl;
-    ratfrac2.reduce2nonreducable();    
-    cout << ratfrac2 << endl;
+    ratfrac2.print('n');
+    ratfrac2.print('d');
+    cout << endl;
+
+    cout << endl << "Comparison check: " << endl;
     cout << (ratfrac2 == ratfrac1) << endl;
     cout << (ratfrac2 >= ratfrac1) << endl;
     cout << (ratfrac2 <= ratfrac1) << endl;
