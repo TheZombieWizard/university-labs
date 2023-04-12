9d536145d655b24729706cc15ef86ded555b4c62
ZombieWizard <64956872+TheZombieWizard@users.noreply.github.com>
2023-04-12 18:34:08 +0300

    Update 2.5.cpp
    Add a couple of lines for deleting the minimum element.

--- C++_Projects/2 term/OOP/2.5/2.5.cpp
+++ C++_Projects/2 term/OOP/2.5/2.5.cpp
@@ -7,6 +7,10 @@ int main()
     for (int i = 0; i < 42; i++)
         tree.add_node();
     tree.print();
+    cout << endl;
+
+    tree.delete_min_node();
+    tree.print();
     
     return 0;
 }
