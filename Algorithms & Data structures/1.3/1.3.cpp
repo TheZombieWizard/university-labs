9629d6cdded9e0f4f561d5eca330a8fbe5623bd5
ZombieWizard <64956872+TheZombieWizard@users.noreply.github.com>
2023-04-19 15:00:29 +0300

    Update Algorithms 1.3
    Solution with continually merged arrays.
    O(nm), memory (2nm).

--- Algorithms & Data structures/1.3/1.3.cpp
+++ Algorithms & Data structures/1.3/1.3.cpp
@@ -2,38 +2,45 @@
 
 using namespace std;
 
-void merge(){;}
 
 int main() 
 {
     short n, m;
     cin >> n >> m;
-    int* pointers_array[n];
-    int array[n][m];
-    int answer[n*m];
+    int* array = new int[m];
+
+    for (int i = 0; i < m; ++i)
+        cin >> array[i];
     
-    for (int i = 0; i < n; i++)
-        pointers_array[i] = &array[i][0];
-    
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; ++i)
     {
-        for (int q = 0; q < m; q++)
-            cin >> array[i][q];
-    }
-    
-    for (int i = 0; i < n*m; i++)
-    {
-        int min = 0;
-        for (int q = 0; q < n; q++)
-            if (*pointers_array[q] < *pointers_array[min])
-                min = q;
-        answer[i] = *pointers_array[min];
-        pointers_array[min]++;
-        if (pointers_array[min] - pointers_array[])
+        int arr_index = 0;
+        int input_arr_index = 0;
+        int* new_array = new int[m*(i+1)];
+        int* input_array = new int[m];
+        
+        for (int q = 0; q < m; ++q)
+            cin >> input_array[q];
+        
+        for (int q = 0; q < m*(i+1); ++q)
+        {
+            if (arr_index >= m*i)
+                new_array[q] = input_array[input_arr_index++];
+            else if (input_arr_index >= m)
+                new_array[q] = array[arr_index++];
+            else if (input_array[input_arr_index] <= array[arr_index])
+                new_array[q] = input_array[input_arr_index++];
+            else
+                new_array[q] = array[arr_index++];
+        }
+
+        delete[] array;
+        delete[] input_array;
+        array = new_array;
     }
 
-    for (int i = 0; i < n*m; i++)
-        cout << answer[i] << " ";
-
+    for (int i = 0; i < n*m; ++i)
+        cout << array[i] << " ";
+    
     return 0;
 }
