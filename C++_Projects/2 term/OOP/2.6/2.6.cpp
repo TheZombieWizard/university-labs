39e4491a88af5540e819740326e1b871e7a3dba3
ZombieWizard <64956872+TheZombieWizard@users.noreply.github.com>
2023-04-19 15:04:27 +0300

    Update OOP 2.6.cpp
    Add more advanced checks for the BigBinaryNum class.

--- C++_Projects/2 term/OOP/2.6/2.6.cpp
+++ C++_Projects/2 term/OOP/2.6/2.6.cpp
@@ -4,6 +4,8 @@
 int main()
 {
     BigBinaryNum bigbin("101010");
+    cout << "Enter binary num: " << endl;
+    cin >> bigbin;
     cout << bigbin << endl;
     cout << bigbin.bin2dec() << "  //Conversion" << endl;
 
@@ -12,9 +14,12 @@ int main()
     cout << endl << "Operations test: " << endl;
     BigBinaryNum operations_test("101");
     cout << "  " << bigbin << " and " << operations_test << endl;
-    cout << "  " << (bigbin - operations_test) << "  //Substraction" << endl;
+    cout << "  " << bigbin.bin2dec() << " and " << operations_test.bin2dec() << endl;
+    cout << "  " << (bigbin - operations_test) << "  //Substraction binary" << endl;
+    cout << "  " << (bigbin - operations_test).bin2dec() << "  //Substraction decimal" << endl;
     cout << "  " << (bigbin / operations_test) << "  //Subdivision" << endl;
-    cout << "  " << (bigbin % operations_test) << "  //Remainder division" << endl;
+    cout << "  " << (bigbin % operations_test) << "  //Remainder division binary" << endl;
+    cout << "  " << (bigbin % operations_test).bin2dec() << "  //Remainder division decimal" << endl;
 
 
 
@@ -28,6 +33,11 @@ int main()
     BigBinaryNum right_offset_test("1010101111");
     right_offset_test > 5;
     cout << "  " << right_offset_test << "  //Right offset" << endl;    
-    
+
+    BigBinaryNum task;
+    task.resize(201);
+    task[0] = 1;
+    cout << "//" << task.bin2dec();
+
     return 0;
 }
