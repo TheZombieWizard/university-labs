1899bf54639c9f64494c22934011d435fa8d94c9
biscuit-lord <64956872+biscuit-lord@users.noreply.github.com>
2023-10-31 10:37:14 +0300

    Added LDLt factorisation

--- numerical_methods_python/lab1_linear_systems.py
+++ numerical_methods_python/lab1_linear_systems.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 
 # TODO:
 # 1. A * x = b      -> x'
@@ -17,11 +16,12 @@ def find_main_element(matrix, column):
             line = i
     return line
 
-
 def solve_linear_system_gauss(matrix, vector):
     matrix_size = len(matrix)
     # Make the values of the matrix and the vector <= 1.0.
     max_num = max([max(i) for i in matrix])
+    if max_num == 0:
+        raise ValueError("Zero divison. There is no solution to this linear system.")
     matrix = [[num/max_num for num in matrix[line]] for line in range(matrix_size)]
     vector = [num/max_num for num in vector]
     
@@ -32,7 +32,7 @@ def solve_linear_system_gauss(matrix, vector):
             matrix[k], matrix[i] = matrix[i], matrix[k]
             vector[k], vector[i] = vector[i], vector[k]
         if matrix[i][i] == 0:
-            raise ValueError("")
+            raise ValueError("Zero divison. There is no solution to this linear system.")
         vector[i] = vector[i] / matrix[i][i]
         matrix[i] = [num/matrix[i][i] for num in matrix[i]]
         for q in range(i+1, matrix_size):
@@ -58,8 +58,54 @@ def solve_linear_system_gauss(matrix, vector):
     return matrix_parameters
 
 
+def solve_linear_system_ldlt_factorisation(matrix, vector):
+    # - Матрица должна быть симметричной
+    # O(n^3 / 6)
+    # + Метод является устойчивым к ошибкам округления и 
+    # не требует перестановок строк и столбцов.
+    # + По времени решения приблизительно в 2 раза быстрее метода Гаусса
+    matrix_size = len(matrix)
+    L = [[0 for _ in range(matrix_size)] for _ in range(matrix_size)]
+    D = [[0 for _ in range(matrix_size)] for _ in range(matrix_size)]
+    Lt = None
+
+    for i in range(matrix_size):
+        D[i][i] = matrix[i][i] - sum([D[k][k]*L[i][k]*L[i][k] for k in range(i)])
+        L[i][i] = 1
+        for q in range(i+1, matrix_size):
+            substraction_unit = 0
+            for m in range(i):
+                substraction_unit = D[m][m]
+                for k in range(m, matrix_size):
+                    substraction_unit *= L[k][m]
+            L[q][i] = (matrix[q][i] - substraction_unit) / D[i][i]
+    
+    L = np.array(L)
+    D = np.array(D)
+    Lt = np.transpose(L)
+
+    print(L)
+    print(D)
+    print(Lt)
+
+    y_solution = [0 for _ in range(matrix_size)]
+    for i in range(matrix_size):
+        y_solution[i] = (vector[i] - sum([L[i][k] * y_solution[k] for k in range(i)])) / L[i][i]
+
+    z_solution = [0 for _ in range(matrix_size)]
+    for i in range(matrix_size):
+        z_solution[i] = y_solution[i] / D[i][i]
+    
+    x_solution = [0 for _ in range(matrix_size)]
+    for i in range(matrix_size-1, -1, -1):
+        x_solution[i] = (z_solution[i] - sum([Lt[i][k] * x_solution[k] for k in range(i+1, matrix_size)])) / Lt[i][i]
+
+    print(y_solution)
+    print(z_solution)
+    print("Solution (x): ", x_solution)
+
 while True:
-    test_case = int(input("Number of the test case: "))
+    test_case = input("Number of the test case: ")
     matrix_A = []
     vector_b = None
     with open("lab1_test_cases.txt", "r") as test_file:
@@ -74,19 +120,23 @@ while True:
             matrix_A.append([float(num) for num in test_file.readline().split()])
         vector_b = [float(num) for num in test_file.readline().split()]
     
-    solution = solve_linear_system_gauss(matrix_A, vector_b)
-    print(f"Solution: {solution}")
-    
-    vector_b_reverse_ingeneered = np.matmul(matrix_A, solution)
-    print(f"Reverse ingeneered b: {vector_b_reverse_ingeneered}")
-    
-    error_vector = np.subtract(vector_b_reverse_ingeneered, vector_b)
-    print(f"Error vector: {error_vector}")
-    max_abs_error_val = max([abs(i) for i in error_vector])
-    print(f"Error vectors' norm: {max_abs_error_val}")
-    
-    solution_with_error = solve_linear_system_gauss(matrix_A, vector_b_reverse_ingeneered)
-    print(f"Solution with error: {solution_with_error}")
-    
-    relative_error = max([abs(i) for i in np.subtract(solution_with_error, solution)]) / max([abs(i) for i in solution])
-    print(f"Relative error: {relative_error}")
+    method = int(input("Method (Gauss - 0, LDLt - 1): "))
+    if not method:
+        solution = solve_linear_system_gauss(matrix_A, vector_b)
+        print(f"Solution: {solution}")
+
+        vector_b_reverse_ingeneered = np.matmul(matrix_A, solution)
+        print(f"Reverse ingeneered b: {vector_b_reverse_ingeneered}")
+
+        error_vector = np.subtract(vector_b_reverse_ingeneered, vector_b)
+        print(f"Error vector: {error_vector}")
+        max_abs_error_val = max([abs(i) for i in error_vector])
+        print(f"Error vectors' norm: {max_abs_error_val}")
+
+        solution_with_error = solve_linear_system_gauss(matrix_A, vector_b_reverse_ingeneered)
+        print(f"Solution with error: {solution_with_error}")
+
+        relative_error = max([abs(i) for i in np.subtract(solution_with_error, solution)]) / max([abs(i) for i in solution])
+        print(f"Relative error: {relative_error}")
+    else:
+        solve_linear_system_ldlt_factorisation(matrix_A, vector_b)
